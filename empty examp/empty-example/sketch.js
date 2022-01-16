03c48f53a82521f2f3e6b89c6c1cd2d3225ff6bb
ApertureSight <97801261+ApertureSight@users.noreply.github.com>
2022-01-16 12:47:32 -0500

    Separation of Sketch and Robot practices

--- empty examp/empty-example/sketch.js
+++ empty examp/empty-example/sketch.js
@@ -1,15 +1,15 @@
-//function setup() {
+function setup() {
   createCanvas(480 , 120)
 }
 
-//function draw() {
+function draw() {
   background(211);
   point(240, 60);
   point(479, 119);
   point(1, 1);
   point(1, 119);
   point(479, 1);
- // creates a diagonal line from bottom left to top right, passing through the middle
+//  creates a diagonal line from bottom left to top right, passing through the middle
   line(1, 119, 479, 1);
 //creates a right triangle in the upper right quadrant
   triangle(120, 1, 120, 80, 80, 80);
@@ -31,46 +31,3 @@
 // arc(390, 60, 80, 80, radians(45), radians(225));
 //
 }
-
- 
-function setup2() {
-  createCanvas(720, 480);
-  strokeWeight(2);
-  ellipseMode(RADIUS);
-}
-
-function draw2() {
-  background(204);
-
-  // Neck
-  stroke(102);                // Set stroke to gray
-  line(266, 257, 266, 162);   // Left
-  line(276, 257, 276, 162);   // Middle
-  line(286, 257, 286, 162);   // Right
-  //antennae
-  line(276, 155, 246, 112);   // Small
-  line(276, 155, 306, 56);    // Tall
-  line(276, 155, 342, 170);   // Medium
-
-  // Body
-  noStroke();                 // Disable stroke
-  fill(102);                  // Set fill to gray
-  ellipse(264, 377, 33, 33);  // Antigravity orb
-  fill(0);                    // Set fill to black
-  rect(219, 257, 90, 120);    // Main body
-  fill(102);                  // Set fill to gray
-  rect(219, 274, 90, 6);      // Gray stripe
-
-  // Head
-  fill(0);                    // Set fill to black
-  ellipse(276, 155, 45, 45);  // Head
-  fill(255);                  // Set fill to white
-  ellipse(288, 150, 14, 14);  // Large eye
-  fill(0);                    // Set fill to black
-  ellipse(288, 150, 3, 3);    // Pupil
-  fill(153);                  // Set fill to light gray
-  ellipse(263, 148, 5, 5);    // Small eye 1
-  ellipse(296, 130, 4, 4);    // Small eye 2
-  ellipse(305, 162, 3, 3);    // Small eye 3
-
-} 
