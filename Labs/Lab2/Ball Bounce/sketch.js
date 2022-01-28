fbb7080aea2144117a353138c31667e59deaba4b
ApertureSight <97801261+ApertureSight@users.noreply.github.com>
2022-01-27 20:13:26 -0500

    Update sketch.js

--- Labs/Lab2/Ball Bounce/sketch.js
+++ Labs/Lab2/Ball Bounce/sketch.js
@@ -1,29 +1,40 @@
+//ADam Wright 
+//1-27-2022
+//N220
 
+
+//global variables for x and y position of ball
 let x;
 let y;
-
+//global variables to control the movement of the ball
 let xspeed;
 let yspeed;
 
 function setup(){
+  
   createCanvas(800,600);
+  //sets the starting point for the ball
   x=400;
   y=300;
-  xspeed =10;
-  yspeed =10;
+  //sets the speed of the ball
+  xspeed =7;
+  yspeed =7;
+  //adjusted the frame rate to make it smoother
   frameRate(24);
 }
 
 function draw(){
   background(0);
-  speed(.5);
 
-  fill(0,250,250)
+//made the color
+  fill(0,250,250);
+  //created an interactive ellipse
   ellipse(x,y,80,60);
-  
+  //moves the ball along x coordinates
   x = x+ xspeed;
+  //moves the ball along y coordinates
   y= y+yspeed;
-  
+  //creates the bounce or limiting effect, also changes direction by multiplying by -1
   if (x + 30 >= width || x - 30 <= 0){
     xspeed = xspeed *-1;
     
