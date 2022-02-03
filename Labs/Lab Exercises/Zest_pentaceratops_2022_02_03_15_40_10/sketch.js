218d10b4db2f24b102ebecf8d999e550dfd5840a
ApertureSight <97801261+ApertureSight@users.noreply.github.com>
2022-02-03 11:19:13 -0500

    lab updates

--- Labs/Lab Exercises/Zest_pentaceratops_2022_02_03_15_40_10/sketch.js
+++ Labs/Lab Exercises/Zest_pentaceratops_2022_02_03_15_40_10/sketch.js
@@ -1,34 +1,37 @@
-let aou = { x: 0, y: 0 };
+let position = { x: 0, y: 0 };
 
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
+  //creates a grey background
   background(220);
-  
+  //sets position of line beginning to mouse position
   if(mouseIsPressed) {
-    aou.x = mouseX;
-    aou.y = mouseY;
+    position.x = mouseX;
+    position.y = mouseY;
   
-  
-  let zah = distance( aou, { x: mouseX, y: mouseY });
-  
+  }
+  //determines the distance after mouse pressed
+  let zah = distance( position, { x: mouseX, y: mouseY });
+
+  //changes the color of the line to red if longer than 120 pixels
+
   if(zah > 120) {
     stroke("#FF0000");
-  } else if (zah <= 120){
+  } else (zah < 120){
     stroke("#000000");
   }
-  
-  line(aou.x, aou.y, mouseX, mouseY);
+  //determines the distance between start and finish
+  line(position.x, position.y, mouseX, mouseY);
 }
-
+//function to determine distance
 function distance(p1, p2) {
   
   let dx = p1.x - p2.x;
   let dy = p1.y - p2.y;
   
   //return the distance
-   Math.sqrt(dx*dx + dy*dy);
-}
+   return Math.sqrt(dx*dx + dy*dy);
 }
