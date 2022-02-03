fd6298e1f0a0eb696399b97b92c1dd46019b40b2
ApertureSight <97801261+ApertureSight@users.noreply.github.com>
2022-02-03 18:35:50 -0500

    comments

--- Labs/Lab3/drawingDrawer/sketch.js
+++ Labs/Lab3/drawingDrawer/sketch.js
@@ -1,20 +1,27 @@
+//Adam Wright
+//2/3/2022
+//N220 Tues/Thursday 
 
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
+
+  //sets the grey background
   background(220);
 
+
+//pulling the drawing function out of my draawing drawer
   alienHead(100,25);
   alienHead(-100,25);
   alienHead(0, 25);
   
   
-}
+}//my drawing function that creates the alien head
   function alienHead(x,y){
-    
     push();
+    //reads the x and y coordinates and in combination with the push, sends them to each drawing to be made
     translate(x,y);
 //face
   fill(255,255,255);
@@ -27,6 +34,6 @@ function draw() {
 //smile
     fill(255,255,255);
   arc(200, 200, 70,70, QUARTER_PI, PI);
-    
+    //dunno what this does, but it made it work
     pop();
   }
