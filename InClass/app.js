164bbceb3a6b55caf21bc1308ca3e31cd17f1d20
ApertureSight <97801261+ApertureSight@users.noreply.github.com>
2022-03-01 11:35:57 -0500

    Midterm

--- InClass/app.js
+++ InClass/app.js
@@ -1,8 +1,23 @@
 
-JOURNAL = [1,34,5,6,7,7,8];
 
 
-for (let i = 0; i < JOURNAL.length; i++) {
-  let entry = JOURNAL[i];
-console.log(JOURNAL.i);
+function countTwenties(numberArray){
+  var count = 0;
+ 
+  for (var i = 0; i < numberArray.length; i++){
+  
+  if(numberArray[i] > 20){
+    var count = count +1; 
+  }
+
+  console.log(count);
+  }
+
+  countTwenties([2,3,4,70,27,18,44]);
 }
+
+
+    
+
+
+  
