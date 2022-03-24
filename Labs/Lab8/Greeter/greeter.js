10742827c7ef10df1d7d52988cc6c7f4c82d1d70
ApertureSight <97801261+ApertureSight@users.noreply.github.com>
2022-03-24 19:17:40 -0400

    Lab8 Inputs

---
+++ Labs/Lab8/Greeter/greeter.js
@@ -0,0 +1,8 @@
+let txtName = document.getElementById('name');
+let dvStoryResult = document.getElementById('nameResult');
+
+
+function processName(){
+    let word = txtName.value;
+    console.log(`Hello ${word}!`);
+}
